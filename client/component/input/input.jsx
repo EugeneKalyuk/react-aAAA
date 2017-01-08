e0815e1c5a3b17a03bf9745d967d0d9559247483
kalyuk_e@magora-systems.com <68LbdVsi>
2017-01-08 22:40:41 +0700

    asd

--- client/component/input/input.jsx
+++ client/component/input/input.jsx
@@ -1,30 +1,66 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {bindActionCreators} from "redux"
 
 if (global.IS_BROWSER) {
 	require('./input.styl');
 }
-let element;
-function timer(){
+let name = document.getElementById('name');
+let nameError = document.getElementById('name-error');
+let text = document.getElementById('text');
+let textError = document.getElementById('text-error');
 
+console.log(nameError)
 
-	console.log(new Date.getSeconds());
+function checkName(){
+    if(!name.value.length){
+        nameError.classList.addClass('input-field__error_active')
+    }
 }
-setInterval(timer, 1000);
+function checkText() {
+    if(!text.value.length){
+        textError.classList.addClass('input-field__error_active')
+    }
+}
+
 class InputCommit extends Component {
 
+	constructor(props) {
+        super(props);
+        this.state = {
+        	payload: 'empty'
+		};
+    }
 
+    handelForm() {
+        checkText();
+		checkName();
+		if(name.value.length && text.value.length){
+            this.setState({
+                payload: 'fill'
+            })
+		} else {
+            this.setState({
+                payload: 'empty'
+            })
+		}
+    };
+
+	
+	
 	render() {
+        console.log(this.state);
 		return (
 		<div className="container">
 			<div className="row">
 				<div className="col-md-12 input">
 					<label htmlFor="name" className="input-field__label">Name:</label>
 					<input type="text" id="name" className="input__input-field input-field input-field_name" />
-					<label htmlFor="name" className="input-field__error">Field is required</label>
+					<label htmlFor="name" className="input-field__error" id="name-error">Field is required</label>
 					<label htmlFor="name" className="input-field__label">Message:</label>
 					<textarea rows="5" id="text" className="input__input-field input-field input-field_text" />
-					<label htmlFor="name" className="input-field__error">Field is required</label>
-					<button type="button" className="input__input-btn input-btn btn btn-default">Send</button>
+					<label htmlFor="name" className="input-field__error" id="text-error">Field is required</label>
+					<button type="button" className="input__input-btn input-btn btn btn-default" onClick={ this.handelForm.bind(this) }>Send</button>
 				</div>
 			</div>
 		</div>
